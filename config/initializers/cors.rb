47ccfd52c1a2a65508278049b91e1435f8fd405b
Josh Whitney <josh.whitney18@gmail.com>
2016-11-06 17:14:14 -0500

    add subdomain to heroku to prevent CORS errors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'herokuapp.com', 'largress-api.herokuapp.com', 'paulsfootpalace.com'
+    origins 'localhost:8080', 'herokuapp.com', 'largress-api.herokuapp.com', 'paulsfootpalace.com', 'largess.joshua-whitney.com'
 
     resource '*',
       headers: :any,
